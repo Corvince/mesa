89554554bdf4b6385bcc0790903926abb64431ce
Corvince <vincent.hess@posteo.de>
2017-12-15 10:14:37 +0100

    Ongoing work

--- mesa/visualization/templates/js/runcontrol.js
+++ mesa/visualization/templates/js/runcontrol.js
@@ -25,6 +25,7 @@ var player; // Variable to store the continuous player
 var control = new MesaVisualizationControl();
 var elements = [];  // List of Element objects
 var model_params = {};
+var reporters = [];
 
 // Playback buttons
 var playPauseButton = $('#play-pause');
@@ -44,12 +45,13 @@ var sidebar = $("#sidebar");
 
 // WebSocket Stuff
 // Open the websocket connection; support TLS-specific URLs when appropriate
-var ws = new WebSocket((window.location.protocol == "https:" ? "wss://" : "ws://") + 
+var ws = new WebSocket((window.location.protocol == "https:" ? "wss://" : "ws://") +
     location.host + "/ws");
 
 ws.onopen = function() {
     console.log("Connection opened!");
     send({"type": "get_params"}); // Request model parameters when websocket is ready
+    send({"type": "get_reporters"});
     reset();
 };
 
@@ -195,17 +197,50 @@ var initGUI = function() {
                 break;
         }
     }
+
+    var addReporter = function(reporters) {
+
+        var dom_id = 'reporter'
+        sidebar.append(
+          "<p><label for='" + dom_id + "' class='label label-primary'>Reporter</label></p>"
+        )
+
+        var select = $("<select class='custom-select' id='reporter-selector'></select>")
+        var option_selected = $("<option selected>Select reporter</option>")
+        select.append(option_selected)
+        for (var i =0; i < reporters.length; i++) {
+          var option = $("<option value='" + i + "'>" + reporters[i] + "</option>")
+          select.append(option)
+        }
+        sidebar.append(select)
+
+    }
+
+
+    addReporter(reporters)
+    var chartButton = $('<button type="button" class="btn btn-secondary" onclick="createChart()">Create chart</button>')
+    sidebar.append(chartButton)
+
+
 };
+
+var createChart = function() {
+    var e = $("#reporter-selector")[0];
+    var label = e.options[e.selectedIndex].text;
+    send({"type": "create_chart", "label": label})
+}
 
 /** Parse and handle an incoming message on the WebSocket connection. */
 ws.onmessage = function(message) {
     var msg = JSON.parse(message.data);
+    console.log(msg)
     switch (msg["type"]) {
         case "viz_state":
             var data = msg["data"];
             for (var i in elements) {
                 elements[i].render(data[i]);
             }
+            console.log(data)
             break;
         case "end":
             // We have reached the end of the model
@@ -218,6 +253,10 @@ ws.onmessage = function(message) {
         case "model_params":
             console.log(msg["params"]);
             model_params = msg["params"];
+            break;
+        case "model_reporters":
+            console.log(msg["reporters"])
+            reporters = msg["reporters"]
             initGUI();
             break;
         default:
@@ -289,3 +328,41 @@ playPauseButton.on('click', run);
 stepButton.on('click', step);
 resetButton.on('click', reset);
 fpsControl.on('change', updateFPS);
+
+function sortable(rootEl) {
+   var dragEl;
+   var target
+
+   // Making all siblings movable
+   [].slice.call(rootEl.children).forEach(function (itemEl) {
+       itemEl.draggable = true;
+       itemEl.addEventListener('dragstart', onDragStart, false)
+       itemEl.addEventListener('dragover', onDragOver, false);
+       itemEl.addEventListener('dragend', onDragEnd, false);
+   });
+
+   function onDragStart(evt){
+       dragEl = evt.target; // Remembering an element that will be moved
+
+       // Limiting the movement type
+       evt.dataTransfer.effectAllowed = 'move';
+       evt.dataTransfer.setData('Text', dragEl.id);
+   }
+
+   // Function responsible for sorting
+   function onDragOver(evt) {
+       evt.preventDefault();
+       evt.dataTransfer.dropEffect = 'move';
+       target = evt.target
+       evt.target.style.border = '4px dotted'
+   }
+
+   // End of sorting
+   function onDragEnd(evt){
+       evt.preventDefault();
+       console.log(rootEl)
+       console.log(dragEl)
+       console.log(target)
+       rootEl.insertBefore(dragEl, target)
+   }
+}
